ee86f8ced3c0eb2b949dbdb4372f032e746f6d39
pauloabraao <abraaoteles@alu.ufc.br>
2025-12-07 09:39:01 -0300

    feat(app): added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard/Dashboard";
+
 import MissionsHome from "./pages/Missions/HomeSpace/Mission";
 import MissionsPublic from "./pages/Missions/PublicSpace/Mission";
+import MissionsConsumption from "./pages/Missions/ConsumptionMission/Mission";
+import MissionsWork from "./pages/Missions/WorkMission/Mission";
+import MissionsMobility from "./pages/Missions/MobilityMission/Mission";
+
+import Challenges from "./pages/Challenges/HomeSpace/Challenges";
 
 function App() {
   return (
@@ -14,6 +20,11 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/mission/1" element={<MissionsPublic />} />
         <Route path="/mission/2" element={<MissionsHome />} />
+        <Route path="/mission/3" element={<MissionsConsumption />} />
+        <Route path="/mission/4" element={<MissionsWork />} />
+        <Route path="/mission/5" element={<MissionsMobility />} />
+
+        <Route path="/challenges/1" element={<Challenges />} />
       </Routes>
     </BrowserRouter>
   );
